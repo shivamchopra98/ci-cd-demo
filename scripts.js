78512e111d390970ae907dd5ee2480c704f714d4
Shivam Chopra <shivam.chopra@infoservices.com>
2025-11-14 15:46:13 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("actionBtn").addEventListener("click", () => {
+  alert("Hello from your static site on Vercel!");
+  console.log("Button clicked — script working!");
+});
+ 
